c9e6d0989cf76dedd8faae4d2af201fc069ed6e8
gab.abasolo <31750757+gabrielleabasolo@users.noreply.github.com>
2021-09-04 02:47:11 +0800

    Added new feats
    -login
    -logout
    -dashboard
    -register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,17 +7,30 @@
     <title>Document</title>
 </head>
 <body class="bg-gray-300 bg">
-    <nav class="flex justify-between p-6 mb-6 bg-white">
+    <nav class="flex justify-between p-3 mb-6 bg-white">
         <ul class="flex items-center">
-            <li><a href="" class="p-6">Home</a></li>
-            <li><a href="" class="p-6">Dashboard</a></li>
-            <li><a href="" class="p-6">Post</a></li>
+            <li><a href="" class="p-3">Home</a></li>
+            <li><a href="{{ route('dashboard') }}" class="p-3">Dashboard</a></li>
+            <li><a href="" class="p-3">Post</a></li>
         </ul>
         <ul class="flex items-center">
-            <li><a href="" class="p-6">Hi Gab</a></li>
-            <li><a href="" class="p-6">Log in</a></li>
-            <li><a href="" class="p-6">Register</a></li>
-            <li><a href="" class="p-6">Logout</a></li>
+            @auth
+            <li><a href="" class="p-3">Hi Gab</a></li>
+            <li>
+                <form action="{{ route('logout') }}" method="post" class="inline">
+                    @csrf
+                    <button type="submit">Log Out</button>
+                </form>
+            </li>
+            @endauth
+            
+            @guest
+            <li><a href="{{ route('login') }}" class="p-3">Log in</a></li>
+            <li><a href="{{ route('register') }}" class="p-3">Register</a></li>
+            @endguest
+            
+            
+            
         </ul>
     </nav>
     @yield('content')
